c1a8ed25adf2e4137f5dda2ce4abc9bb9f1aafb5
Jandy Alberto Mayta Cari <48447165+JAlbertoMC@users.noreply.github.com>
2023-09-07 09:12:54 -0500

    v1.2
    Ahora omite datos incompletos y duplicados

--- tarea3_1.py
+++ tarea3_1.py
@@ -8,28 +8,33 @@ response = requests.get(url)
 
 if response.status_code == 200:
     soup = BeautifulSoup(response.content, 'html.parser')
-
-    # BUSCA ELEMENTOS CON CLASE "jeg_post_titulo" para titulos
+    
     titulos = soup.find_all(class_='jeg_post_title')
-
-    # BUSCA ELEMENTOS CON CLASE "jeg_meta_fecha" para fechas
     fechas = soup.find_all(class_='jeg_meta_date')
 
-    # Verifica que haya títulos y fechas disponibles
     if titulos and fechas:
         # Abre un archivo CSV para escribir
-        with open('Titulares_LosAndes.csv', 'w', newline='', encoding='utf-8-sig') as csvfile:
+        with open('Titular4.csv', 'w', newline='', encoding='utf-8-sig') as csvfile:
             archivo = csv.writer(csvfile)
 
             # Escribe el encabezado del CSV
             archivo.writerow(['TITULAR DEL DIA', 'FECHA'])
+            
+            # Utiliza un conjunto para realizar un seguimiento de los datos ya agregados
+            datos_agregados = set()
 
             # Itera a través de los títulos y fechas y escribe en el CSV
             for titulo, fecha in zip(titulos, fechas):
                 titular_hoy = titulo.get_text(strip=True)
                 fecha_titular = fecha.get_text(strip=True)
-                archivo.writerow([titular_hoy, fecha_titular])
-
+                
+                # Verificar que en cada fila tenga TITULO Y FECHA 
+                if titular_hoy and fecha_titular:
+                    fila = (titular_hoy, fecha_titular)
+                    if fila not in datos_agregados:
+                        archivo.writerow([titular_hoy, fecha_titular])
+                        datos_agregados.add(fila)
+                    
         print("Archivo CSV creado correctamente.")
     else:
         print("No se encontraron títulos o fechas en la página.")
